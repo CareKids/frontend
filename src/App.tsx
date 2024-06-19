e8ee5b522e523b581ed4b411275f3be139eedf8c
eter2 <eter2hy@gmail.com>
2024-06-19 11:32:44 +0900

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './static/App.css'
+import Header from './components/Header'
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+    </div>
+  );
+}
+
+export default App;
