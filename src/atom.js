fff3f67ff132b4c24f3d43676345ec81a430e2e3
eter2 <eter2hy@gmail.com>
2024-07-10 15:57:16 +0900

    chore: bug fix

--- src/atom.js
+++ src/atom.js
@@ -2,10 +2,10 @@ import { atom } from 'recoil';
 
 export const loginState = atom({
   key: 'loginState',
-  default: true,
+  default: false,
 });
 
 export const userRoleState = atom({
   key: 'userRoleState',
-  default: 'admin', // 일반사용자: user, 관리자: admin
+  default: 'user', // 일반사용자: user, 관리자: admin
 });
