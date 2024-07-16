fa684613bcc54ad8be9c7dc21f9e27c0f5719aee
eter2 <eter2hy@gmail.com>
2024-07-16 13:19:57 +0900

    feat: link policy api
    policy list page
    policy detail page

--- src/pages/Policy.tsx
+++ src/pages/Policy.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Button, Input, InputGroup, Card, CardBody, CardTitle, CardText, Row, Col, Container } from 'reactstrap';
 import { Pagination, PaginationItem, PaginationLink } from 'reactstrap';
@@ -7,50 +7,129 @@ import { faSearch, faFilter, faChevronLeft, faChevronRight } from '@fortawesome/
 
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import { getPolicyData, filterPolicyData, getRegions, getAgeTags } from '../api/load';
+import { PolicyBoardInfo, PolicyItem, Region, AgeTag } from '../api/types';
 
-interface Policies {
-  id: number;
-  title: string;
-  type: string;
-  content: string;
-}
+const formatTime = (time: number): string => {
+  return time.toString().padStart(2, '0');
+};
 
+const ITEMS_PER_PAGE = 12;
 const Policy: React.FC = () => {
-  const [searchTerm, setSearchTerm] = useState('');
-  const [currentPage, setCurrentPage] = useState(1);
-  const postsPerPage = 10;
-
-  // filter 버튼 클릭 여부
+  const [policyInfo, setPolicyInfo] = useState<PolicyBoardInfo | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
   const [showFilters, setShowFilters] = useState(false);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [selectedRegion, setSelectedRegion] = useState<Region | null>(null);
+  const [selectedAge, setSelectedAge] = useState<AgeTag | null>(null);
+  const [ages, setAges] = useState<AgeTag[]>([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [regions, setRegions] = useState<Region[]>([]);
+
+  useEffect(() => {
+    fetchRegions();
+    fetchAges();
+    fetchInitialPolicyData();
+  }, []);
+
+  useEffect(() => {
+    searchAndFilter();
+  }, [selectedRegion, selectedAge]);  
+
+  const fetchRegions = async () => {
+    try {
+      const regionData = await getRegions();
+      setRegions(regionData);
+    } catch (err) {
+      console.error('Failed to fetch regions:', err);
+      setError('지역 정보를 불러오는 데 실패했습니다.');
+    }
+  };
+
+  const fetchAges = async () => {
+    try {
+      const ageData = await getAgeTags();
+      setAges(ageData);
+    } catch (err) {
+      console.error('Failed to fetch ages:', err);
+      setError('나이 정보를 불러오는 데 실패했습니다.');
+    }
+  };
+
+  const fetchInitialPolicyData = async () => {
+    setLoading(true);
+    try {
+      const data = await getPolicyData(currentPage, ITEMS_PER_PAGE);
+      setPolicyInfo(data);
+
+      if (!selectedRegion && data.region) {
+        setSelectedRegion(data.region);
+      }
+
+      if (!selectedAge && data['age-tag']) {
+        setSelectedAge(data['age-tag'])
+      }      
+    } catch (err) {
+      setError('정책 정보를 불러오는 데 실패했습니다.');
+    } finally {
+      setLoading(false);
+    }
+  };
+
   const toggleFilters = () => {
     setShowFilters(!showFilters);
   };
-    
-  // 임시 데이터
-  const policies: Policies[] = [
-    {
-      id: 1,
-      title: '모르면 손해 보는 2024년 달라지는 육아...',
-      type: '강남구',
-      content: '【베이비뉴스 김솔미 기자】 엄마아빠가 꼭 알아야 할 2024년 달라지는 육아 정책 13가지, 싹 다 모아봤어. 1. 첫만남이용권 바우처 다자녀 가구 지원 확대...'
-    },
-    {
-      id: 2,
-      title: '2024년 달라지는 육아정책 13가지',
-      type: '강서구',
-      content: '생애 초기 아동이 충분한 돌봄을 받을 수 있도록 부모급여 지원금액이 확대됐어. 지원대상은 0~1세 아동으로 0세는 월 70만 원에서 100만 원으로, 1세는 월 35만 원에서 50만 원으로 상향됐어. 지급방식은 현금 또는 바우처인데, 어린이집을 이용하는 경우 보육료...'
-    },
-  ]
 
-  const filteredPosts = policies.filter((post) =>
-    post.title.toLowerCase().includes(searchTerm.toLowerCase())
-  );
+  const searchAndFilter = async (page: number = currentPage) => {
+    if (!searchTerm && !selectedRegion) {
+      return fetchInitialPolicyData();
+    }
 
-  const handleSearch = () => {
-    // 검색하기 버튼 클릭 시 실행
+    setLoading(true);
+    try {
+      const data = await filterPolicyData(
+        { 
+          query: searchTerm || null, 
+          region: selectedRegion || {},
+          "age-tag": selectedAge || {}
+        }, 
+        page
+      );
+      setPolicyInfo(data);
+      setCurrentPage(page);
+    } catch (err) {
+      setError('검색에 실패했습니다.');
+    } finally {
+      setLoading(false);
+    }
   };
 
-  const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
+  const handleSearchAndFilter = () => {
+    searchAndFilter(1);
+  };
+
+  const handleRegionChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const regionId = Number(event.target.value);
+    const selectedRegion = regions.find(region => region.id === regionId) || null;
+    setSelectedRegion(selectedRegion);
+  };
+
+  const handleAgeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const ageId = Number(event.target.value);
+    const selectedAge = ages.find(age => age.id === ageId) || null;
+    setSelectedAge(selectedAge);
+  };
+
+  const paginate = (pageNumber: number) => {
+    searchAndFilter(pageNumber);
+  };
+  
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>{error}</div>;
+  if (!policyInfo) return <div>No data available</div>;
+
+  const totalPages = policyInfo.pageInfo.total;
 
   return (
     <div className='App'>
@@ -58,92 +137,116 @@ const Policy: React.FC = () => {
       <Container className="mt-4">
         <h1 className="mb-4"><b>정책</b></h1>
 
-        {/* 검색창 */}
         <div className="bg-white rounded-4 p-3 mb-4">
           <InputGroup>
-            <Input placeholder="검색어 입력" className="border-1" />
+            <Input 
+              placeholder="검색어 입력" 
+              className="border-1" 
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+            />
             <Button color="primary" className="me-2" onClick={toggleFilters}>
               <FontAwesomeIcon icon={faFilter} className="me-2 rounded-4" />
               필터
             </Button>
-            <Button color="primary" onClick={handleSearch}>
+            <Button color="primary" onClick={handleSearchAndFilter}>
               <FontAwesomeIcon icon={faSearch} className="me-2 rounded-4" />
               검색하기
             </Button>
           </InputGroup>
         </div>
 
-        {/* 필터 */}
         {showFilters && (
           <div className="bg-light p-3 mb-4 rounded">
             <Row>
               <Col md={6}>
-                <Input type="select" className="mb-2">
-                  <option>지역</option>
-                  {/* TODO: 지역 목록 불러오기 */}
+                <Input 
+                  type="select" 
+                  value={selectedRegion?.id || ''} 
+                  onChange={handleRegionChange}
+                >
+                  {regions.map((region) => (
+                    <option key={region.id} value={region.id}>{region.name}</option>
+                  ))}
                 </Input>
               </Col>
               <Col md={6}>
-                <Input type="select">
-                  <option>연령대</option>
-                  {/* TODO: 연령대 목록 불러오기 */}
+                <Input 
+                  type="select" 
+                  value={selectedAge?.id || ''} 
+                  onChange={handleAgeChange}
+                >
+                  {ages.map((age) => (
+                    <option key={age.id} value={age.id}>{age.name}</option>
+                  ))}
                 </Input>
               </Col>
             </Row>
           </div>
         )}
 
-        <div className="mb-3">
-          <div>검색 결과 {policies.length}건</div>
-        </div>
-        
-        {/* 정책 목록 리스트 */}
         <Row>
-          {policies.map((result, index) => (
-            <Col md={4} key={index} className="mb-4">
-                <Card className="h-100">
-                    <Link to={`/policy/${result.id}`} style={{ textDecoration: 'none', color: 'inherit' }}>
-                        <CardBody>
-                            <Row>
-                                <Col>
-                                    <Button color="secondary" size="sm" className="mb-2" disabled>{result.type}</Button>
-                                    <CardTitle tag="h5" className="mb-2"><strong>{result.title}</strong></CardTitle>
-                                    <CardText>
-                                        {result.content}
-                                    </CardText>
-                                </Col>
-                            </Row>
-                        </CardBody>
-                    </Link>
-                </Card>
-            </Col>
+          {policyInfo.pageList.map((item: PolicyItem) => (
+            <div className="col-md-4 mb-4" key={item.id}>
+              <div className="card" style={{ borderRadius: '10px', backgroundColor: '#ffffff' }}>
+                <Link to={`/policy/${item.id}`} style={{ textDecoration: 'none', color: 'inherit' }}>
+                  <div className="card-body">
+                      <h4 className="card-title"
+                          style={{
+                              whiteSpace: 'pre-wrap',
+                              wordBreak: 'break-word',
+                              overflow: 'hidden',
+                              textOverflow: 'ellipsis',
+                              display: '-webkit-box',
+                              WebkitLineClamp: 1,
+                              WebkitBoxOrient: 'vertical',
+                          }}>{item.title}</h4>
+                      <div className="card-text mb-2" 
+                          style={{
+                              whiteSpace: 'pre-wrap',
+                              wordBreak: 'break-word',
+                              overflow: 'hidden',
+                              textOverflow: 'ellipsis',
+                              display: '-webkit-box',
+                              WebkitLineClamp: 5,
+                              WebkitBoxOrient: 'vertical',
+                              minHeight: '100px',
+                              maxHeight: '100px',
+                              fontSize: '14px'
+                          }}>
+                          {item.description}
+                      </div>
+                    </div>
+                </Link>
+              </div>
+          </div>
           ))}
-        </Row>        
-                
+        </Row>
+        
         <nav aria-label="Page navigation" className="mt-4 bg-transparent">
           <ul className="pagination justify-content-center">
-              <li className={`page-item ${currentPage === 1 ? 'disabled' : ''}`}>
-              <button className="page-link text-primary" onClick={() => paginate(currentPage - 1)}>
-                  <FontAwesomeIcon icon={faChevronLeft} />
+            <li className={`page-item ${currentPage === 1 ? 'disabled' : ''}`}>
+              <button className="page-link text-primary" onClick={() => paginate(currentPage - 1)} disabled={currentPage === 1}>
+                <FontAwesomeIcon icon={faChevronLeft} />
               </button>
-              </li>
-              {Array.from({ length: Math.ceil(filteredPosts.length / postsPerPage) }).map((_, index) => (
+            </li>
+            {Array.from({ length: totalPages }).map((_, index) => (
               <li key={index} className={`page-item ${currentPage === index + 1 ? 'active' : ''}`}>
-                  <button 
-                  onClick={() => paginate(index + 1)} 
+                <button
+                  onClick={() => paginate(index + 1)}
                   className={`page-link ${currentPage === index + 1 ? 'bg-primary border-primary' : 'text-primary'}`}
-                  >
+                >
                   {index + 1}
-                  </button>
+                </button>
               </li>
-              ))}
-              <li className={`page-item ${currentPage === Math.ceil(filteredPosts.length / postsPerPage) ? 'disabled' : ''}`}>
-              <button className="page-link text-primary" onClick={() => paginate(currentPage + 1)}>
-                  <FontAwesomeIcon icon={faChevronRight} />
+            ))}
+            <li className={`page-item ${currentPage === totalPages ? 'disabled' : ''}`}>
+              <button className="page-link text-primary" onClick={() => paginate(currentPage + 1)} disabled={currentPage === totalPages}>
+                <FontAwesomeIcon icon={faChevronRight} />
               </button>
-              </li>
+            </li>
           </ul>
-      </nav>
+        </nav>
       </Container>
       <Footer />
     </div>    
--- src/pages/PolicyDetail.tsx
+++ src/pages/PolicyDetail.tsx
@@ -1,61 +1,111 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
-import { Container, Card, CardBody, Button } from 'reactstrap';
+import { Container, Button } from 'reactstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faList, faDownload } from '@fortawesome/free-solid-svg-icons';
+import { faList} from '@fortawesome/free-solid-svg-icons';
 
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-
-interface Post {
-  id: string;
-  title: string;
-  type: string;
-  content: string;
-  date: string;  
-}
+import { getPolicyDetailData } from '../api/load';
+import { DetailPolicyItem } from '../api/types';
 
 const PlayDetail: React.FC = () => {
+  const [policyItem, setPolicyItem] = useState<DetailPolicyItem | null>(null);
   const { id } = useParams<{ id: string }>();
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
   const navigate = useNavigate();
 
-  const post: Post = {
-    id: id || '',
-    title: '모르면 손해 보는 2024년 달라지는 육아정책 13가지',
-    type: '강남구',
-    content: '엄마아빠가 꼭 알아야 할 2024년 달라지는 육아 정책 13가지, 싹 다 모아봤어. 1. 다자녀 가구 지원 확대 출생 초기 양육비용 경감을 위한 첫만남이용권 바우처가 2024년 1월 1일부터 첫째아는 200만 원, 둘째아 이상 출생아는 300만 원으로 확대됐어. ',
-    date: '2024-06-25'
-  }
+  useEffect(() => {
+    if (!id) {
+      setError('유효하지 않은 ID입니다.');
+      setLoading(false);
+      return;
+    }
+
+    const fetchItem = async () => {    
+      setLoading(true);
+      try {
+        const policyData = await getPolicyDetailData(id);
+        setPolicyItem(policyData);
+      } catch (err) {
+        console.error('Failed to fetch policy data:', err);
+        setError('정책 정보를 불러오는 데 실패했습니다.');
+      } finally {
+        setLoading(false);
+      }
+    };
+    
+    fetchItem();
+  }, [id]);
 
   const handleGoBack = () => {
     navigate(-1);
   };
+  
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>{error}</div>;
+  if (!policyItem) return <div>No data available</div>;
 
   return (
     <div className='App'>
         <Header></Header>
         <Container className="mt-4">
-            <Button
-            color="secondary"
-            outline
-            size="sm"
-            className="mb-2 rounded-pill"
-            style={{
-                fontSize: '0.8rem',
-                borderColor: '#6c757d',
-                color: '#6c757d',
-                pointerEvents: 'none' 
-            }}
-            >
-            {post.type}
-            </Button>
-            <h1 className="mb-2 mt-2"><strong>{post.title}</strong></h1>
-            <div>{post.date}</div>
+            {policyItem.region.map((region) => (
+              <Button
+                color="secondary"
+                outline
+                size="sm"
+                className="mb-2 rounded-pill"
+                style={{
+                    fontSize: '0.8rem',
+                    borderColor: '#6c757d',
+                    color: '#6c757d',
+                    pointerEvents: 'none' 
+                }}
+                >
+                {region.name}
+              </Button>
+            ))}
+            <h1 className="mb-2 mt-2"><strong>{policyItem.title}</strong></h1>
                 
             <hr style={{ border: 'none', height: '2px', backgroundColor: '#dddddd' }} />
-            <div>{post.content}</div>
+            <div className='mb-1'><strong>대상 연령</strong></div>
+            <div className='mb-4'>
+            {policyItem['age-tag'].map((tag) => (
+              <Button
+                color="secondary"
+                outline
+                size="sm"
+                className="mb-2 me-2"
+                style={{
+                    fontSize: '0.8rem',
+                    borderColor: '#6c757d',
+                    color: '#6c757d',
+                    pointerEvents: 'none' 
+                }}
+                >
+                {tag.name}
+              </Button>
+            ))}
+            </div>
+            <div><strong>정책 대상</strong></div>
+            <div className='mb-4' style={{ whiteSpace: 'pre-wrap',  wordBreak: 'break-word' }}>{policyItem.target}</div> <div><strong>정책 내용</strong></div>
+            <div className='mb-4' style={{ whiteSpace: 'pre-wrap',  wordBreak: 'break-word' }}>{policyItem.text}</div>            
+            <div><strong>신청 방법</strong></div>
+            <div className='mb-4' style={{ whiteSpace: 'pre-wrap',  wordBreak: 'break-word' }}>{policyItem.process}</div>             
+            <div><strong>바로 가기</strong></div>
+            <div className='mb-4' style={{ whiteSpace: 'pre-wrap', wordBreak: 'break-word' }}>
+              <a 
+                href={policyItem.url} 
+                target="_blank" 
+                rel="noopener noreferrer"
+                style={{ textDecoration: 'underline', color: 'inherit' }}
+              >
+                홈페이지
+              </a>
+            </div>
             <hr style={{ border: 'none', height: '2px', backgroundColor: '#dddddd' }} />
-
             <div className="text-center mt-4">
             <Button color="primary" onClick={handleGoBack}>
                 <FontAwesomeIcon icon={faList} className="me-2" />
